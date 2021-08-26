7f0266a87600647e1eb225820b500207dc8a4922
Chathurika Senani <zenaronline@gmail.com>
2021-08-26 21:50:10 +0530

    Create Eureka Discovery Server

--- src/main/java/com/appsdeveloperlog/photoapp/discovery/photoappdiscoveryservice/PhotoappdiscoveryserviceApplication.java
+++ src/main/java/com/appsdeveloperlog/photoapp/discovery/photoappdiscoveryservice/PhotoappdiscoveryserviceApplication.java
@@ -2,8 +2,10 @@ package com.appsdeveloperlog.photoapp.discovery.photoappdiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PhotoappdiscoveryserviceApplication {
 
 	public static void main(String[] args) {
