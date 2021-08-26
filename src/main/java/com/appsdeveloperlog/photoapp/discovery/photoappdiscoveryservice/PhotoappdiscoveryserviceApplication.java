c43a9f6d1d8e0ded8ab6b475e54a206aca576eef
Chathurika Senani <zenaronline@gmail.com>
2021-08-26 19:54:35 +0530

    Spring Boot Initialized

---
+++ src/main/java/com/appsdeveloperlog/photoapp/discovery/photoappdiscoveryservice/PhotoappdiscoveryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperlog.photoapp.discovery.photoappdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappdiscoveryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoappdiscoveryserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appsdeveloperlog/photoapp/discovery/photoappdiscoveryservice/PhotoappdiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperlog.photoapp.discovery.photoappdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappdiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
