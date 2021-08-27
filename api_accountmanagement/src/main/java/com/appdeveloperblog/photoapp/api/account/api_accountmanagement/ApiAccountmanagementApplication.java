5eda187f44735a81ca8ed90f28d29867c3896b28
Chathurika Senani <zenaronline@gmail.com>
2021-08-27 12:24:04 +0530

    Account Management Micro Service initialized

---
+++ api_accountmanagement/src/main/java/com/appdeveloperblog/photoapp/api/account/api_accountmanagement/ApiAccountmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.appdeveloperblog.photoapp.api.account.api_accountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAccountmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAccountmanagementApplication.class, args);
+	}
+
+}
---
+++ api_accountmanagement/src/test/java/com/appdeveloperblog/photoapp/api/account/api_accountmanagement/ApiAccountmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdeveloperblog.photoapp.api.account.api_accountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAccountmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
