970cd6964002b35d29a3156905c6d741e536689d
Chathurika Senani <zenaronline@gmail.com>
2021-08-29 02:10:06 +0530

    Http Status code returned

--- api_users/src/main/java/com/appdeveloperblog/photoapp/api/users/api_users/ui/controllers/UsersController.java
+++ api_users/src/main/java/com/appdeveloperblog/photoapp/api/users/api_users/ui/controllers/UsersController.java
@@ -8,6 +8,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,14 +32,14 @@ public class UsersController {
     }
 
     @PostMapping
-    public String createUser(@RequestBody CreateUserRequestModel userDetails) {
+    public ResponseEntity createUser(@RequestBody CreateUserRequestModel userDetails) {
 
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserDto userDto = modelMapper.map(userDetails, UserDto.class);
         userService.createUser(userDto);
-        
-        return "Post user";
+
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
