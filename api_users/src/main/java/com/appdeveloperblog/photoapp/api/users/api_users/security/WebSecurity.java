acc0dd69092019a77a44123469a943c3a4689acb
Chathurika Senani <zenaronline@gmail.com>
2021-08-31 22:19:14 +0530

    Web Security enabled

--- api_users/src/main/java/com/appdeveloperblog/photoapp/api/users/api_users/security/WebSecurity.java
+++ api_users/src/main/java/com/appdeveloperblog/photoapp/api/users/api_users/security/WebSecurity.java
@@ -1,15 +1,29 @@
 package com.appdeveloperblog.photoapp.api.users.api_users.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter{
+
+    Environment environment;
+
+    @Autowired
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     public void configure(HttpSecurity http)
             throws Exception {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/users/**").permitAll();
+        // http.authorizeRequests().antMatchers("/**").hasIpAddress("gateway.ip"); // To Enable gateway IP
         http.headers().frameOptions().disable();
     }
 }
--- api_users/src/main/java/com/appdeveloperblog/photoapp/api/users/api_users/ui/controllers/UsersController.java
+++ api_users/src/main/java/com/appdeveloperblog/photoapp/api/users/api_users/ui/controllers/UsersController.java
@@ -10,6 +10,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,14 +33,15 @@ public class UsersController {
         return "working";
     }
 
-    @PostMapping
+    @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+    produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<CreateUserResponseModel> createUser(@RequestBody CreateUserRequestModel userDetails) {
 
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserDto userDto = modelMapper.map(userDetails, UserDto.class);
-        userService.createUser(userDto);
+        // userService.createUser(userDto);
 
         UserDto createdUser = userService.createUser(userDto);
 
