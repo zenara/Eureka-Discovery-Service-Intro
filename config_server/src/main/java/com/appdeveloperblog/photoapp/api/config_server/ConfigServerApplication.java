8b1b2dbc6d48b07a79f77c6a3c7a277315b5436e
Chathurika Senani <zenaronline@gmail.com>
2021-09-02 00:02:34 +0530

    Config server setup

--- config_server/src/main/java/com/appdeveloperblog/photoapp/api/config_server/ConfigServerApplication.java
+++ config_server/src/main/java/com/appdeveloperblog/photoapp/api/config_server/ConfigServerApplication.java
@@ -1,9 +1,13 @@
 package com.appdeveloperblog.photoapp.api.config_server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableAutoConfiguration
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
