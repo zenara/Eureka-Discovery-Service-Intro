fe9c502684ab90e89af45427284fe6aae290b8ba
Chathurika Senani <zenaronline@gmail.com>
2021-09-01 14:16:58 +0530

    Login Url path updated -> users/login

--- api_users/src/main/java/com/appdeveloperblog/photoapp/api/users/api_users/security/WebSecurity.java
+++ api_users/src/main/java/com/appdeveloperblog/photoapp/api/users/api_users/security/WebSecurity.java
@@ -40,6 +40,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
     private AuthenticationFilter getAuthenticationFilter() throws Exception  {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(userService, environment, authenticationManager());
         // authenticationFilter.setAuthenticationManager(authenticationManager());
+        authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
         return authenticationFilter;
     }
 
